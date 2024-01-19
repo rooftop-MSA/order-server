b19d33d3e3c54d901cc5d7210375a7312f18c929
devxb <dlwnsdud205@naver.com>
2024-01-19 17:49:52 +0900

    feat: OrderController의 confirmOrder를 구현한다

--- src/main/kotlin/org/rooftop/order/controller/OrderController.kt
+++ src/main/kotlin/org/rooftop/order/controller/OrderController.kt
@@ -2,9 +2,11 @@ package org.rooftop.order.controller
 
 import org.rooftop.api.identity.ErrorRes
 import org.rooftop.api.identity.errorRes
+import org.rooftop.api.order.OrderConfirmReq
 import org.rooftop.api.order.OrderReq
 import org.rooftop.api.order.OrderRes
 import org.rooftop.api.order.orderRes
+import org.rooftop.order.app.OrderConfirmFacade
 import org.rooftop.order.app.OrderFacade
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
@@ -14,6 +16,7 @@ import reactor.core.publisher.Mono
 @RequestMapping("/v1")
 class OrderController(
     private val orderFacade: OrderFacade,
+    private val orderConfirmFacade: OrderConfirmFacade,
 ) {
 
     @PostMapping("/orders")
@@ -25,6 +28,12 @@ class OrderController(
                     this.orderId = it.id
                 }
             }
+    }
+
+    @PostMapping("/orders/confirms")
+    @ResponseStatus(HttpStatus.OK)
+    fun confirmOrder(@RequestBody orderConfirmReq: OrderConfirmReq): Mono<Unit> {
+        return orderConfirmFacade.confirmOrder(orderConfirmReq)
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
