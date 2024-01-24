37135defc6d14311dee94533fe8eddf0bd4ff45c
xb205 <62425964+devxb@users.noreply.github.com>
2024-01-24 17:13:47 +0900

    refactor: 서버 통신 uri 환경변수로 분리 (#17)

--- src/main/kotlin/org/rooftop/order/infra/WebClientConfigurer.kt
+++ src/main/kotlin/org/rooftop/order/infra/WebClientConfigurer.kt
@@ -1,5 +1,6 @@
 package org.rooftop.order.infra
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
@@ -7,14 +8,18 @@ import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
 @Profile("prod")
-class WebClientConfigurer {
+class WebClientConfigurer(
+    @Value("\${rooftop.server.identity:http://identity.rooftopmsa.org}") private val identityServerUri: String,
+    @Value("\${rooftop.server.shop:http://shop.rooftopmsa.org}") private val shopServerUri: String,
+    @Value("\${rooftop.server.pay:http://pay.rooftopmsa.org}") private val payServerUri: String,
+) {
 
     @Bean
-    fun identityWebClient(): WebClient = WebClient.create("http://identity.rooftopmsa.org")
+    fun identityWebClient(): WebClient = WebClient.create(identityServerUri)
 
     @Bean
-    fun shopWebClient(): WebClient = WebClient.create("http://shop.rooftopmsa.org")
+    fun shopWebClient(): WebClient = WebClient.create(shopServerUri)
 
     @Bean
-    fun payWebClient(): WebClient = WebClient.create("http://pay.rooftopmsa.org")
+    fun payWebClient(): WebClient = WebClient.create(payServerUri)
 }
